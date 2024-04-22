7699f11428c927869735d31cf24a201de731b988
Iyxan23 <nurihsanalghifari@gmail.com>
2024-04-22 14:20:02 +0700

    feat: impl seeding of patients n schedules

--- next-dashboard/scripts/seed.ts
+++ next-dashboard/scripts/seed.ts
@@ -1,6 +1,19 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 
-// yeah these kinda weird
+import {
+  addDays,
+  addWeeks,
+  differenceInDays,
+  format,
+  getDate,
+  getDay,
+  getMonth,
+  getYear,
+  subWeeks,
+} from "date-fns";
+import { patient, schedule } from "../src/server/db/schema";
+
+// yeah these are kinda weird
 (await import("dotenv")).config();
 const {
   default: { hash },
@@ -8,12 +21,75 @@ const {
 const { db } = await import("../src/server/db");
 const { users } = await import("../src/server/db/schema");
 
+function choice<T>(list: T[]): T {
+  const chosen = list[Math.round(Math.random() * (list.length - 1))];
+  if (!chosen) throw new Error("unreachable");
+  return chosen;
+}
+
 async function main() {
   await db.transaction(async (t) => {
     await t.insert(users).values({
       username: "user",
       password: await hash("p4ssw0rd", 12),
     });
+
+    // create patients
+    const patients = [];
+    for (let a = 0; a < 10; a++) {
+      patients.push(
+        // biome-ignore lint/style/noNonNullAssertion: this is intentional
+        (
+          await db
+            .insert(patient)
+            .values({
+              username: `patient_${a}`,
+              password: await hash(`password${a}`, 12),
+            })
+            .returning()
+        )[0]!,
+      );
+    }
+
+    // create quite some schedules
+    const today = new Date();
+    const startDate = subWeeks(today, 2);
+    const endDate = addWeeks(today, 2);
+
+    const hourStart = 8;
+    const hourEnd = 17;
+
+    for (let day = 0; day < differenceInDays(endDate, startDate); day++) {
+      const thisDay = addDays(startDate, day);
+      let hour = hourStart;
+      while (hour < hourEnd) {
+        let newHour = hour + choice([1, 2, 3]);
+        if (hourEnd < newHour) newHour = hourEnd;
+
+        const type = choice(["break", "appointment"]);
+        await db.insert(schedule).values({
+          dateYear: getYear(thisDay),
+          dateMonth: getMonth(thisDay),
+          dateDay: getDate(thisDay),
+          startHour: hour,
+          startMinute: 0,
+          endHour: newHour,
+          endMinute: 0,
+          isBreak: type === "break",
+          patientId: type === "appointment" ? choice(patients).id : null,
+          title: type === "appointment" ? `appointment ${day}` : null,
+          status: "appointed",
+        });
+        console.log(
+          `inserted ${type} on ${format(
+            thisDay,
+            "yyyy-MM-dd",
+          )} at ${hour}:00-${newHour}:00`,
+        );
+
+        hour = newHour;
+      }
+    }
   });
 }
 
--- next-dashboard/src/lib/utils.ts
+++ next-dashboard/src/lib/utils.ts
@@ -1,6 +1,17 @@
 import { type ClassValue, clsx } from "clsx";
+import type { schemaDate } from "./schemas/schedule";
+import type { z } from "zod";
 import { twMerge } from "tailwind-merge";
+import { getDate, getMonth, getYear } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function dateToScheduleDate(date: Date): z.infer<typeof schemaDate> {
+  const year = getYear(date);
+  const month = getMonth(date);
+  const day = getDate(date);
+
+  return { year, month, day };
 }
