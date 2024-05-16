65f421558a97ea324f2029dc95b1391d71b281f2
Iyxan23 <nurihsanalghifari@gmail.com>
2024-05-17 05:24:52 +0700

    chore: remove searchbar

--- next-dashboard/src/app/dashboard/layout.tsx
+++ next-dashboard/src/app/dashboard/layout.tsx
@@ -36,16 +36,6 @@ export default function DashboardLayout({
             <span className="sr-only">Home</span>
           </Link>
           <div className="w-full flex-1">
-            <form>
-              <div className="relative">
-                <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-gray-500 dark:text-gray-400" />
-                <Input
-                  className="w-full bg-white shadow-none appearance-none pl-8 md:w-2/3 lg:w-1/3 dark:bg-gray-950"
-                  placeholder="Search products..."
-                  type="search"
-                />
-              </div>
-            </form>
           </div>
           <UserProfileDropdown />
         </header>
