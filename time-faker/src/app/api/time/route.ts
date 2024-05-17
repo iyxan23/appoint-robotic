23ecad06ea7703b9a735685bcc10a728609cb91d
Iyxan23 <nurihsanalghifari@gmail.com>
2024-05-17 10:52:59 +0700

    fix: cors on time-faker

--- time-faker/src/app/api/time/route.ts
+++ time-faker/src/app/api/time/route.ts
@@ -3,5 +3,14 @@ import { TimeGetResponse } from "./schema";
 import { getOffsettedTime } from "~/time/time";
 
 export async function GET(): Promise<NextResponse<TimeGetResponse>> {
-  return NextResponse.json({ date: getOffsettedTime(new Date()) });
+  return NextResponse.json(
+    { date: getOffsettedTime(new Date()) },
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    },
+  );
 }
